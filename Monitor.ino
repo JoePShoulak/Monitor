994a8a14315f1b555516762d8c42e35b7966052e
Joe P Shoulak <joe@squarecompass.io>
2024-03-18 17:19:05 -0500

    polish

--- Monitor.ino
+++ Monitor.ino
@@ -1,40 +1,44 @@
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <Eventually.h>
 #include "CircularBuffer.h"
+#include <U8x8lib.h>
 // #include "MenuItem.h"
 
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define SCREEN_RESET -1
-#define SCREEN_ADDRESS 0x3C
-#define DEFAULT_CHANNEL 256
 #define SELECT_BTN 12
 #define CYCLE_BTN 11
 
-int channel = DEFAULT_CHANNEL;  // 1-125
-EvtManager mgr(true);           // true to manage memory
+EvtManager mgr(true);  // true to manage memory
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, SCREEN_RESET);
-
-CircularBuffer<long, 2> baudRates;
+CircularBuffer<String, 4> menuItem;
 CircularBuffer<String, 6> messages;
-CircularBuffer<String, 3> modes;
-CircularBuffer<String, 3> menuItems;
+CircularBuffer<long, 2> baudRate;
+CircularBuffer<String, 3> mode;
+CircularBuffer<int, 3> channel;
+
+U8X8_SSD1306_128X64_NONAME_HW_I2C display(U8X8_PIN_NONE);
 
 bool selectButtonAction() {
-  // TODO: implement
+  menuItem.next();
+  drawMenu();
+
   return true;
 }
 
 bool cycleButtonAction() {
-  // TODO: This should change which menu item is selected, not just cycle the baudrate
-  baudRates.next();
-  Serial.println(baudRates.current());
-  // Serial.end();
-  // Serial.begin(baudRates.current());
-  // updateDisplay();
+  String selection = menuItem.current();
+
+  if (selection == "BR") {
+    baudRate.next();
+    // TODO: Change the serial port
+  } else if (selection == "MODE") {
+    mode.next();
+    // TODO: Change the mode}
+  } else if (selection == "CH") {
+    channel.next();
+    // TODO: Change the channel
+  }
+
+  drawMenu();
 
   return true;
 }
@@ -42,79 +46,62 @@ bool cycleButtonAction() {
 EvtPinListener selectButtonListener(SELECT_BTN, 100, (EvtAction)selectButtonAction);
 EvtPinListener cycleButtonListener(CYCLE_BTN, 100, (EvtAction)cycleButtonAction);
 
-void displaySettings() {
-  display.setCursor(0, 0);
-
-  display.print("BR:");
-  display.print(baudRates.current());
-  display.print(" ");
-
-  display.print(modes.current());
-
-  display.print(" ");
-
-  display.print(channel);
-  display.print(" ");
-
-  display.drawFastHLine(0, 10, SCREEN_WIDTH, SSD1306_WHITE);
-}
-
 void addData(String datum) {
   datum.trim();
   messages.append(datum);
 }
 
-void displayData() {
-  display.setCursor(0, 12);
+template<typename T, int N>
+void printMenuItem(int x, const CircularBuffer<T, N>& buffer, String name, String prefix = "") {
+  display.setInverseFont(menuItem.current() == name);
 
-  for (auto& msg : messages) {
-    display.println(msg);
-  }
+  if (prefix)
+    display.drawString(x++, 0, prefix.c_str());
+
+  display.drawString(x, 0, String(buffer.current()).c_str());
 }
 
-void updateDisplay() {
-  display.clearDisplay();
-  displaySettings();
-  displayData();
-  display.display();
+void drawMenu() {
+  display.drawString(0, 0, "                ");
+
+  printMenuItem(0, baudRate, "BR", "B");
+  printMenuItem(8, mode, "MODE");
+  printMenuItem(12, channel, "CH", "C");
+
+  display.setInverseFont(0);
+  display.drawString(0, 1, "----------------");
 }
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("\n=== New Session ===\n");
-
-  // if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-  //   Serial.println("Screen init failed");
-  //   while (true)
-  //     ;
-  // }
+  display.begin();
+  display.setPowerSave(0);
+  display.setFont(u8x8_font_chroma48medium8_r);
 
   pinMode(SELECT_BTN, INPUT);
   pinMode(CYCLE_BTN, INPUT);
+
   mgr.addListener(&selectButtonListener);
   mgr.addListener(&cycleButtonListener);
 
-  baudRates.append(9600);
-  baudRates.append(115200);
+  baudRate.append(9600);
+  baudRate.append(115200);
 
-  Serial.println(baudRates.current());
+  mode.append("MN");
+  mode.append("TX");
+  mode.append("RX");
 
-  modes.append("MON");
-  modes.append("TX");
-  modes.append("RX");
+  channel.append(1);
+  channel.append(2);
+  channel.append(3);
 
-  Serial.println(modes.current());
+  menuItem.append("NONE");
+  menuItem.append("BR");
+  menuItem.append("MODE");
+  menuItem.append("CH");
 
-  menuItems.append("baudRate");
-  menuItems.append("mode");
-  menuItems.append("channel");
+  Serial.begin(baudRate.current());
 
-  Serial.println(menuItems.current());
-
-  Serial.println("Buffers Populated");
-
-  // display.setTextColor(SSD1306_WHITE);
-  // updateDisplay();
+  drawMenu();
 }
 
 void loop() {
@@ -122,6 +109,5 @@ void loop() {
 
   if (Serial.available()) {
     addData(Serial.readString());
-    // updateDisplay();
   }
 }
